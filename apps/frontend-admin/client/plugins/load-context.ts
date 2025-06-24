a431a032276df2daf0a7aa4678efeb08721c4d39
Vincent Lainé <vincent.laine.utc@gmail.com>
2025-06-24 18:28:36 +0200

    Bug fix 🐛 (#17)
    * fix: check user rights
    
    * bug fix

--- apps/frontend-admin/client/plugins/load-context.ts
+++ apps/frontend-admin/client/plugins/load-context.ts
@@ -1,9 +1,9 @@
-import {useAuthStore} from "~/stores/auth";
-import {useRefDataStore} from "~/stores/refData";
-import type {Departement} from "~/dto/departement.dto";
-import type {Usage} from "~/dto/usage.dto";
-import type {Thematique} from "~/dto/thematique.dto";
-import type {Commune} from "~/dto/commune.dto";
+import type { Commune } from '~/dto/commune.dto';
+import type { Departement } from '~/dto/departement.dto';
+import type { Thematique } from '~/dto/thematique.dto';
+import type { Usage } from '~/dto/usage.dto';
+import { useAuthStore } from '~/stores/auth';
+import { useRefDataStore } from '~/stores/refData';
 
 export default defineNuxtPlugin((nuxtApp) => {
   const authStore = useAuthStore();
@@ -12,21 +12,12 @@ export default defineNuxtPlugin((nuxtApp) => {
   // CHARGEMENT DES DONNEES DE REFERENCE QUAND L'UTILISATEUR EST CONNECTE
   const loadGlobal = async () => {
     if (await authStore.isAuthenticated) {
-      const [
-        fecthDep,
-        fetchUsage,
-        fetchThematique,
-        fetchCommune,
-        fetchZoneAlerteMaxUpdatedAt,
-      ] = await Promise.all(
-        [
-          api.departement.list(),
-          api.usage.list(),
-          api.thematique.list(),
-          api.commune.list(),
-          api.zoneAlerte.getMaxUpdatedAt(),
-        ]
-      );
+      const [fecthDep, fetchUsage, fetchThematique, fetchZoneAlerteMaxUpdatedAt] = await Promise.all([
+        api.departement.list(),
+        api.usage.list(),
+        api.thematique.list(),
+        api.zoneAlerte.getMaxUpdatedAt(),
+      ]);
       if (fecthDep.data.value) {
         useRefDataStore().setDepartements(<Departement[]>fecthDep.data.value);
       }
@@ -36,18 +27,19 @@ export default defineNuxtPlugin((nuxtApp) => {
       if (fetchThematique.data.value) {
         useRefDataStore().setThematiques(<Thematique[]>fetchThematique.data.value);
       }
-      if (fetchCommune.data.value) {
-        useRefDataStore().setCommunes(<Commune[]>fetchCommune.data.value);
-      }
       if (fetchZoneAlerteMaxUpdatedAt.data.value) {
         useRefDataStore().setZoneAlerteMaxUpdatedAt(<string>fetchZoneAlerteMaxUpdatedAt.data.value);
       }
+      const fetchCommune = await api.commune.list();
+      if (fetchCommune.data.value) {
+        useRefDataStore().setCommunes(<Commune[]>fetchCommune.data.value);
+      }
     }
-  }
+  };
   watch(
     () => authStore.isAuthenticated,
     useUtils().debounce(async () => {
       loadGlobal();
-    }, 1)
+    }, 1),
   );
 });
