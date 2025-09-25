30eaba9eb9af7b7feab56330e85853c015dd7c84
Vincent Lainé <vincent.laine.utc@gmail.com>
2025-09-25 10:48:32 +0200

    tippecanoe test

--- apps/backend-admin/src/zone_alerte_computed/zone_alerte_computed_historic.service.ts
+++ apps/backend-admin/src/zone_alerte_computed/zone_alerte_computed_historic.service.ts
@@ -166,7 +166,7 @@ export class ZoneAlerteComputedHistoricService {
       );
       try {
         await this.execPromise(
-          `${path}/tippecanoe_program/bin/tippecanoe -Z4 -z10 -pg -ai -pn -f --drop-densest-as-needed -l zones_arretes_en_vigueur --read-parallel --detect-shared-borders --simplification=10 --output=${path}/${fileNameToSave}.pmtiles ${path}/${fileNameToSave}.geojson`,
+          `${path}/tippecanoe_program/bin/tippecanoe -Z4 -z10 -pg -ai -pn -f --coalesce-densest-as-needed --extend-zooms-if-still-dropping -l zones_arretes_en_vigueur --read-parallel --detect-shared-borders --simplification=20 --output=${path}/${fileNameToSave}.pmtiles ${path}/${fileNameToSave}.geojson`,
         );
         const dataPmtiles = fs.readFileSync(
           `${path}/${fileNameToSave}.pmtiles`,
