c5cfdd53cd562ee8499d45dd15c57da0f7f077af
Samy GHRIBI <ghribi.samy@gmail.com>
2026-06-29 11:58:33 +0200

    Revert "Handle trailing slash on admin login route"
    This reverts commit 6f03539a433e37654e8ef78dbb3dc294e0e2c0ad.

--- apps/frontend-admin/client/middleware/auth.global.ts
+++ apps/frontend-admin/client/middleware/auth.global.ts
@@ -2,9 +2,8 @@ import { useAuthStore } from '~/stores/auth';
 
 export default defineNuxtRouteMiddleware(async (to) => {
   const authStore = useAuthStore();
-  const isConnexionRoute = to.path.replace(/\/$/, '') === '/connexion';
 
-  if (isConnexionRoute) {
+  if (to.path === '/connexion') {
     if (authStore.isAuthenticated) {
       return navigateTo({ path: '/', query: to.query });
     }
