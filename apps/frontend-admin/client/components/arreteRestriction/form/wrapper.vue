32065539681b08265f781c85eb84d1b23358b584
Samy GHRIBI <ghribi.samy@gmail.com>
2026-08-20 17:51:43 +0200

    fix: surface restriction publication failures

--- apps/frontend-admin/client/components/arreteRestriction/form/wrapper.vue
+++ apps/frontend-admin/client/components/arreteRestriction/form/wrapper.vue
@@ -264,7 +264,12 @@ const publishArrete = async (ar: ArreteRestriction): Promise<boolean> => {
     const { data, error } = await api.arreteRestriction.publish(ar.id?.toString(), ar);
 
     if (error.value) {
-      apiErrorHandler.captureClientError(error.value, publicationContext('publish'));
+      apiErrorHandler.showError(
+        error.value,
+        "Impossible de publier l'arrêté de restriction",
+        'La publication n’a pas abouti. Vérifiez les informations puis réessayez.',
+        publicationContext('publish'),
+      );
       return false;
     }
 
